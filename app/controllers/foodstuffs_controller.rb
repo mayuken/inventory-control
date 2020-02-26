a9837cc717a624a0baec261ef7d55358e3d2f69b
mayuken <superbeaver.drum@gmail.com>
2020-02-26 15:29:14 +0900

    add_select_category_id
    form.collection_selectを使用する事で実装できた！

--- app/controllers/foodstuffs_controller.rb
+++ app/controllers/foodstuffs_controller.rb
@@ -2,7 +2,6 @@ class FoodstuffsController < ApplicationController
 
   def index
     @foodstuffs = Foodstuff.all
-
     @categories = Category.all
   end
 
@@ -10,17 +9,36 @@ class FoodstuffsController < ApplicationController
     @foodstuff = Foodstuff.new
     @categories = Category.all
     @category = Category.new
+    @parents = Category.all.order("id ASC")
   end
 
   def create
-    Foodstuff.create(foodstuff_params)
-    redirect_to root_path
+    @foodstuff = Foodstuff.new(foodstuff_params)
+    if @foodstuff.save
+      flash[:notice] = "食材が追加されました！"
+      redirect_to root_path
+    else
+      flash[:alert] = "食材追加に失敗しました…。"
+      redirect_to new_foodstuff_path
+    end
+
   end
 
   def category_create
-    Category.create(category_params)
-    redirect_to new_foodstuff_path
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "カテゴリーが追加されました！"
+      redirect_to new_foodstuff_path
+    else
+      flash[:alert] = "カテゴリー追加に失敗しました…。"
+      redirect_to new_foodstuff_path
+    end
+    
   end
+
+  def edit
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'foodstuffs#index'
-  resources :foodstuffs, only: [:index, :new, :create] do
+  resources :foodstuffs, only: [:index, :new, :create, :edit, :update] do
     collection do
       post :category_create
     end
